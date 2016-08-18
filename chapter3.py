d4a240fcf66b3ac49ec1ef79524905288dda6de0
knoixs <knoixs@gmail.com>
2016-08-18 19:52:20 +0800

    chapter3-4

---
+++ chapter3.py
@@ -0,0 +1,20 @@
+# import os
+# print os.getcwd()
+
+# data = open('sketch.txt')
+# print(data.readline())
+# print(data.readline())
+# data.seek(0)
+
+
+# data=open('sketch.txt')
+# for each_line in data:
+#     # print (not each_line.find(':'))
+#     if not each_line.find(':') == -1:
+#         (role,spoken)=each_line.split(':',1)
+#         print(role,' said: ',spoken)
+# data.close()
+
+
+
+
