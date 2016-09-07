50538e66dfc27b3becf04f97e30ce221d00c9165
knoixs <knoixs@gmail.com>
2016-09-07 17:22:22 +0800

    在Pycharm的settings中设置project的interpreter为python3

--- webapp/webapp/cgi-bin/add_timing_data.py
+++ webapp/webapp/cgi-bin/add_timing_data.py
@@ -10,11 +10,11 @@ addr = os.environ['REMOTE_ADDR']
 host = os.environ['REMOTE_HOST']
 method = os.environ['REQUEST_METHOD']
 cur_time = time.asctime(time.localtime())
-print (host + "," + addr + "," + cur_time + ":" + method + ":" + "file=" + sys.stderr)
+print(host + "," + addr + "," + cur_time + ":" + method + ":", end='', file=sys.stderr)
 
 form = cgi.FieldStorage()
 for each_form_item in form.keys():
-    print (each_form_item + '->' + form[each_form_item].value + file=sys.stderr)
+    print(each_form_item + '->' + form[each_form_item].value, end='', file=sys.stderr)
 
     print (file=sys.stderr)
     print('OK.')
